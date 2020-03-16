570fd88ef3f55573b3d85e2485668c55d3ae071f
sanmaoyou <15989100135@163.com>
2020-03-16 14:01:44 +0800

    11

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "Example",
+    name: "EachNavigationBar",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "Example",
-            targets: ["Example"]),
+            name: "EachNavigationBar",
+            targets: ["EachNavigationBar"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "Example",
+            name: "EachNavigationBar",
             dependencies: []),
         .testTarget(
-            name: "ExampleTests",
-            dependencies: ["Example"]),
+            name: "EachNavigationBarTests",
+            dependencies: ["EachNavigationBar"]),
     ]
 )
